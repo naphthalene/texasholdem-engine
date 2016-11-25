f6bfec7a388ddac9ce2c495b508e56e5f29b171d
Pasha Sadikov <pashalab@gmail.com>
2016-11-25 13:04:20 -0500

    Adds our own bot so we can keep the starter as a reference/benchmark

--- compile_bot.sh
+++ compile_bot.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-javac -d starterBot/bin/ `find starterBot -name '*.java' -regex '^[./A-Za-z0-9]*$'`
+if [ "$#" -ne 1 ]; then
+  echo "Please pass bot directory as argument."
+  echo "./compile_bot.sh <bot_directory>"
+  exit 1
+fi
+
+javac -d "$1"/bin/ `find $1 -name '*.java' -regex '^[./A-Za-z0-9]*$'`
--- run.sh
+++ run.sh
@@ -1,3 +1,13 @@
 #! /bin/bash
 
-java -cp bin com.theaigames.game.texasHoldem.TexasHoldem "java -cp starterBot/bin/ bot.BotStarter" "java -cp starterBot/bin/ bot.BotStarter"
+if [ "$#" -ne 2 ]; then
+  echo "Please pass arguments: bot1, bot2"
+  echo "./compile_bot.sh <starterBot> <myBot>"
+  exit 1
+fi
+
+echo "Don't forget to recompile"
+echo
+echo
+
+java -cp bin com.theaigames.game.texasHoldem.TexasHoldem "java -cp $1/bin/ bot.BotStarter" "java -cp $2/bin/ bot.BotStarter"
